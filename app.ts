a0d3f9dc95a90f9b057928ae4b0bee5d14cf2dc4
Anup <anup.k@turtlemint.com>
2019-06-22 22:20:46 +0530

    enum concept has been added

--- app.ts
+++ app.ts
@@ -6,23 +6,25 @@ console.log("new message has been added...")
 let a = 10
 console.log(a)
 
+enum lanaguesKnown { English,Hindi, marathi}
 //array
 let student ={
 name: "Anup",
-phone:1234567890
+phone:1234567890,
+language: lanaguesKnown[lanaguesKnown.marathi]
 
 }
 
 let studentlist=[
-    {name: "Akshay", phone: 12312312},
+    {name: "Akshay", phone: 12312312, language: lanaguesKnown[lanaguesKnown.English]},
         
-    {name: "amey", phone : 123121212}
+    {name: "amey", phone : 123121212, language: lanaguesKnown[lanaguesKnown.Hindi]}
 ]
 
-//studentlist.push(student)
+studentlist.push(student)
 
 for(var index=0; index< studentlist.length;index++){
     var element = studentlist[index];
-    console.log("Name is: "+element.name+"has phone is: "+element.phone)
+    console.log("Name is: "+element.name+" has phone is: "+element.phone)
 
 }
